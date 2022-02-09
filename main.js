f4626e2cde34f961d07a734f81b08d7c2f12405f
SFA <steve.fallet@divtec.ch>
2022-02-09 09:41:57 +0100

    mini formulaire avec contrôle

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-alert('OK');
+
+let inputNom = document.getElementById('nom');
+let buttonAfficher = document.getElementById('afficher');
+let listeNoms = document.getElementById('listeNoms');
+console.log(listeNoms);
+
+buttonAfficher.addEventListener('click', function () {
+    if (inputNom.value === '') {
+        alert('Nom obligatoire !');
+        return;
+    }
+
+    listeNoms.innerHTML += '<li>' + inputNom.value + '</li>';
+    inputNom.value = '';
+});
